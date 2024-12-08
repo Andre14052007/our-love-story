fc6d3f31dc419337864bbd161c6789cad3c517ec
Andre14052007 <andredsmatos@gmail.com>
2024-12-08 21:03:48 +0000

    mmm

--- our-love-story/astro.config.mjs
+++ our-love-story/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://amor-andre14052007.vercel.app',
+  adapter: vercel(),
 });
